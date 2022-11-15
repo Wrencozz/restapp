02f25b19e79886b0c08a59082bb7f23a5924360d
Wrencozz <fakychips@gmail.com>
2022-11-15 11:50:29 +0100

    stoPercentServer_clientTakPetnact

--- server/index.js
+++ server/index.js
@@ -6,9 +6,10 @@ const app = express();
 const PORT = 3000;
 
 const bookRouter = require("./routes/book");
+const petRouter = require("./routes/pet");
 
 mongoose.connect(
-    `connectionString`,
+    `mongodb+srv://admin:${process.env.MONGODB_PW}@wrencos.0a9c6sj.mongodb.net/store?retryWrites=true&w=majority`,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true
@@ -19,4 +20,6 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 app.use("/book", bookRouter);
+app.use("/pet",petRouter);
+
 app.listen(PORT, () => console.log(`App is running on ${PORT}`));
---
+++ server/models/pet.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const petSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  year: { type: Number, required: true },
+  breed: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Pet", petSchema);
